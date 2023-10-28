0c8eb7028dfe04c124dcfe0be8ff59a4d1cc8ad6
L1s <lebedewgeorg@yandex.ru>
2023-10-29 00:13:58 +0700

    SixthLaboratoryWorkRework - Переработана шестая лабораторная работа.

--- SixthLaboratoryWork/lib/models/Photos.dart
+++ SixthLaboratoryWork/lib/models/Photos.dart
@@ -20,8 +20,7 @@ class Photos {
   Photos.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     sol = json['sol'];
-    camera =
-    json['camera'] != null ? new Camera.fromJson(json['camera']) : null;
+    camera = json['camera'] != null ? new Camera.fromJson(json['camera']) : null;
     imgSrc = json['img_src'];
     earthDate = json['earth_date'];
     rover = json['rover'] != null ? new Rover.fromJson(json['rover']) : null;
--- SixthLaboratoryWork/lib/screens/cubit/NasaState.dart
+++ SixthLaboratoryWork/lib/screens/cubit/NasaState.dart
@@ -1,4 +1,6 @@
+import '../../models/Camera.dart';
 import '../../models/Nasa.dart';
+import '../../models/Rover.dart';
 
 abstract class NasaState {}
 
@@ -6,7 +8,20 @@ class NasaLoadingState extends NasaState {}
 
 class NasaLoadedState extends NasaState {
   Nasa data;
-  NasaLoadedState({required this.data});
+  double offset;
+  NasaLoadedState(this.data, this.offset);
 }
 
 class NasaErrorState extends NasaState {}
+
+class PhotoDetailInitialState extends NasaState {}
+
+class PhotoDetailShownState extends NasaState {
+  Camera? camera;
+  Rover? rover;
+  String? imgSrc;
+  Nasa data;
+  double offset;
+
+  PhotoDetailShownState(this.imgSrc, this.camera, this.rover, this.data, this.offset);
+}
